8a424ca0fb74db8ff03ed1db47dd902a1ee73d02
jennifrmarie <jen@EPICACXIV.attlocal.net>
2020-03-20 16:00:53 -0400

    trying to fix cors for deployment

--- src/app.js
+++ src/app.js
@@ -12,8 +12,20 @@ const app = express()
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
   skip: () => NODE_ENV === 'test',
 }))
+var allowedOrigins = ['http://localhost:8000',
+                      'https://frozen-crag-79266.herokuapp.com/'];
 app.use(cors({
-  origin: 'https://frozen-crag-79266.herokuapp.com/'
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 app.use(helmet())
 
